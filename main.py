7104550d7953af6f63efa395cce78982c1269505
knneth1890 <78786336+knneth1890@users.noreply.github.com>
2023-11-09 12:58:56 +0000

    add outputfile and edit main branch

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print ("first string\nsecond string")
-print ("kenneth"+"tilado")
-print ("kenneth"+" "+"tilado")
+# String manipulation PythonExercise1
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
