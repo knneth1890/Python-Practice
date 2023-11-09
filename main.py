f7c9eb4734c1b113d64d897a5452de6a8a396990
knneth1890 <78786336+knneth1890@users.noreply.github.com>
2023-11-09 14:24:07 +0000

    added input func

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ print("Day 1 - String Manipulation")
 print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
+print("_________________________________________________")
+
+# this is new exercise using input functions
+print(" ")
+print("Hello " + input("what is your name?"))
